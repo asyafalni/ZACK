ea8e3ac39daea98c3f93edcfe578d259c8de63a9
Alfin Syafalni <MacBookAir@eiMac.local>
2016-10-21 09:33:32 +0800

    Initial Commit

---
+++ ZACK/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ZACK
+//
+//  Created by Alfin Syafalni on 21/10/2016.
+//  Copyright © 2016 Sakurai Lab. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
